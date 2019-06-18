54c63791d29b47153f38a5f7dd4a99e40aaaa111
Mateusz Kania <Mateusz577@gmail.com>
2019-06-18 15:39:39 +0200

    Add antd, configured css and scss load module

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import {Button} from "antd";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>My React App!</h1>
+        <Button>My React App!</Button>
       </div>
     );
   }
